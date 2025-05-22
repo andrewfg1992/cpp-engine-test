f4e890ca3d6a589a866405844ced285cb83ed1db
andrewfg1992 <43049699+andrewfg1992@users.noreply.github.com>
2025-05-22 18:22:04 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "MatlabEngine.hpp"
+#include <iostream>
+
+int main() {
+
+    // Start MATLAB engine
+    std::unique_ptr<MATLABEngine> matlabPtr = startMATLAB();
+
+    double result = matlabPtr->feval<double>(u"sqrt", 4.0);
+    if(result==2) {
+        std::cout << "success! result is expected value" << std::endl;
+    }
+}
