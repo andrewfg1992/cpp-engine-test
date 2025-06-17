d5415328cd664e830c51fc802c88db4291161a4b
andrewfg1992 <43049699+andrewfg1992@users.noreply.github.com>
2025-06-17 10:57:01 -0400

    check permissions of matlab binary

--- myBuildScript.m
+++ myBuildScript.m
@@ -1,5 +1,15 @@
 mex -client engine main.cpp
 
 mlroot = matlabroot;
+
+filename = [mlroot '/bin/matlab'];  % replace with your file name
+[status, cmdout] = system(['ls -l ', filename]);
+
+if status == 0
+    disp(cmdout);
+else
+    disp('Error accessing file or file does not exist.');
+end
+
 cmd = ['export LD_LIBRARY_PATH=' mlroot '/extern/bin/glnxa64:' mlroot '/sys/os/glnxa64 && ./main'];
 system(cmd);
