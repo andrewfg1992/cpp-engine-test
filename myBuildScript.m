8fa76a171a673110356294503a58db1dfe053598
andrewfg1992 <43049699+andrewfg1992@users.noreply.github.com>
2025-06-17 16:48:02 -0400

    add check for shell

--- myBuildScript.m
+++ myBuildScript.m
@@ -11,8 +11,11 @@ else
     disp('Error accessing file or file does not exist.');
 end
 
+[status, result] = system('echo "$SHELL"');
+disp(result);
+
 % Try starting MATLAB via binary
-system(binName);
+%system(binName);
 
 %cmd = ['export LD_LIBRARY_PATH=' mlroot '/extern/bin/glnxa64:' mlroot '/sys/os/glnxa64 && ./main'];
 %system(cmd);
