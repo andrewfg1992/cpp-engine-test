89018ba509d11b98fc96235cd3effa15c1c05884
andrewfg1992 <43049699+andrewfg1992@users.noreply.github.com>
2025-06-17 16:22:56 -0400

    Try starting MATLAB via binName

--- myBuildScript.m
+++ myBuildScript.m
@@ -2,8 +2,8 @@ mex -client engine main.cpp
 
 mlroot = matlabroot;
 
-filename = [mlroot '/bin/matlab'];  % replace with your file name
-[status, cmdout] = system(['ls -l ', filename]);
+binName = [mlroot '/bin/matlab'];
+[status, cmdout] = system(['ls -l ', binName]);
 
 if status == 0
     disp(cmdout);
@@ -11,5 +11,8 @@ else
     disp('Error accessing file or file does not exist.');
 end
 
-cmd = ['export LD_LIBRARY_PATH=' mlroot '/extern/bin/glnxa64:' mlroot '/sys/os/glnxa64 && ./main'];
-system(cmd);
+% Try starting MATLAB via binary
+system(binName);
+
+%cmd = ['export LD_LIBRARY_PATH=' mlroot '/extern/bin/glnxa64:' mlroot '/sys/os/glnxa64 && ./main'];
+%system(cmd);
