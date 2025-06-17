0265b3930b432c325816d51cc632ae506b7d36e4
andrewfg1992 <43049699+andrewfg1992@users.noreply.github.com>
2025-06-17 17:14:17 -0400

    Troubleshoot paths

--- myBuildScript.m
+++ myBuildScript.m
@@ -11,11 +11,15 @@ else
     disp('Error accessing file or file does not exist.');
 end
 
+% try querying the shell
 [status, result] = system('echo "$SHELL"');
 disp(result);
 
-% Try starting MATLAB via binary
-%system(binName);
+% Try seeing if MATLAB is on the path
+[status, result] = system('which matlab');
+disp(result);
 
-cmd = ['LD_LIBRARY_PATH=' mlroot '/extern/bin/glnxa64:' mlroot '/sys/os/glnxa64 ./main'];
+cmd = ['LD_LIBRARY_PATH=' mlroot '/bin/glnxa64:' ...
+       mlroot '/extern/bin/glnxa64:' ...
+       mlroot '/sys/os/glnxa64:$LD_LIBRARY_PATH ./main'];
 system(cmd);
