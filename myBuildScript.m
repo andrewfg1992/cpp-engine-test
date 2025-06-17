c6ddfc8760e13fa906ec912604a155a3aff1483e
andrewfg1992 <43049699+andrewfg1992@users.noreply.github.com>
2025-06-17 10:11:54 -0400

    add run step

--- myBuildScript.m
+++ myBuildScript.m
@@ -1,5 +1,5 @@
 mex -client engine main.cpp
 
-%mlroot = matlabroot;
-%cmd = ['export LD_LIBRARY_PATH ' mlroot '/extern/bin/glnxa64:' mlroot '/sys/os/glnxa64 && ./main']
-%system(cmd);
+mlroot = matlabroot;
+cmd = ['export LD_LIBRARY_PATH=' mlroot '/extern/bin/glnxa64:' mlroot '/sys/os/glnxa64 && ./main'];
+system(cmd);
